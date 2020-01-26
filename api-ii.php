3b15ff8550d6da2f3ac5c4e019f8df6119b04830
dtsppk <60294467+dtsppk@users.noreply.github.com>
2020-01-26 09:44:12 +0700

    Update api-ii.php

--- api-ii.php
+++ api-ii.php
@@ -1,82 +1,50 @@
 <?php
+ 
 
-  $strAccessToken = "zj9ogZu/30Rx801lMdNzvRLAoBKXE3HvhxQy2LJExxjsn7LmmQk+7LWBtwtmTetMpubO3wj0WVfjjdxKdreVCpEURekR90KEfQwdGykVDd3GS48wZnYjZx14IEiMLeQkF547WaRrKbQFpFjFIlrcGwdB04t89/1O/w1cDnyilFU=";
-  
-  $content = file_get_contents('php://input');
-  $arrJson = json_decode($content, true);
+$strAccessToken = "zj9ogZu/30Rx801lMdNzvRLAoBKXE3HvhxQy2LJExxjsn7LmmQk+7LWBtwtmTetMpubO3wj0WVfjjdxKdreVCpEURekR90KEfQwdGykVDd3GS48wZnYjZx14IEiMLeQkF547WaRrKbQFpFjFIlrcGwdB04t89/1O/w1cDnyilFU=";
+ 
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+ 
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+if($arrJson['events'][0]['message']['text'] == "hi"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี User ID คุณคือ ".$arrJson['events'][0]['source']['userId']. "กรุณาคัดลอกข้อความนี้ ส่งมาที่ ไลน์ ส่วนตัวของ กายกาก นะครับเพื่อลงทะเบียน";
 
-  $strUrl = "https://api.line.me/v2/bot/message/reply";
-
-  $arrHeader = array();
-  $arrHeader[] = "Content-Type: application/json";
-  $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
-  $_msg = $arrJson['events'][0]['message']['text'];
-
-
-  $api_key="xX3Bhqg9gp1ds9yfrPfgzaa8BlYS2igP";
-  $url = 'https://api.mongolab.com/api/1/databases/data/collections/datas?apiKey='.$api_key.'';
-  $json = file_get_contents('https://api.mongolab.com/api/1/databases/data/collections/datas?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
-  $data = json_decode($json);
-  $isData=sizeof($data);
-
-  if (strpos($_msg, 'H.E.L.E.N') !== false) 
-  {
-    if (strpos($_msg, 'H.E.L.E.N') !== false) 
-    {
-      $x_tra = str_replace("H.E.L.E.N","", $_msg);
-      $pieces = explode("|", $x_tra);
-      $_question=str_replace("[","",$pieces[0]);
-      $_answer=str_replace("]","",$pieces[1]);
-      //Post New Data
-      $newData = json_encode(
-        array(
-          'question' => $_question,
-          'answer'=> $_answer
-        )
-      );
-      $opts = array(
-        'http' => array(
-            'method' => "POST",
-            'header' => "Content-type: application/json",
-            'content' => $newData
-         )
-      );
-      $context = stream_context_create($opts);
-      $returnValue = file_get_contents($url,false,$context);
-      $arrPostData = array();
-      $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-      $arrPostData['messages'][0]['type'] = "text";
-      $arrPostData['messages'][0]['text'] = 'Thank you.';
-    }
-  }
-  else
-  {
-    if($isData >0){
-       foreach($data as $rec){
-        $arrPostData = array();
-        $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-        $arrPostData['messages'][0]['type'] = "text";
-        $arrPostData['messages'][0]['text'] = $rec->answer;
-       }
-    }
-    else
-    {
-        $arrPostData = array();
-        $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-        $arrPostData['messages'][0]['type'] = "text";
-        $arrPostData['messages'][0]['text'] = 'H.E.L.E.N คุณสามารถสอนให้ฉันฉลาดได้เพียงพิมพ์: H.E.L.E.N[คำถาม|คำตอบ]';
-    }
-  }
-
-  $channel = curl_init();
-  curl_setopt($channel, CURLOPT_URL,$strUrl);
-  curl_setopt($channel, CURLOPT_HEADER, false);
-  curl_setopt($channel, CURLOPT_POST, true);
-  curl_setopt($channel, CURLOPT_HTTPHEADER, $arrHeader);
-  curl_setopt($channel, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-  curl_setopt($channel, CURLOPT_RETURNTRANSFER,true);
-  curl_setopt($channel, CURLOPT_SSL_VERIFYPEER, false);
-  $result = curl_exec($channel);
-  curl_close ($channel);
-  echo "sucess full";
+}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
+}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+}else{
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี User ID คุณคือ ".$arrJson['events'][0]['source']['userId']. "กรุณาคัดลอกข้อความนี้ ส่งมาที่ ไลน์ ส่วนตัวของ กาย นะครับเพื่อลงทะเบียน";
+}
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
 ?>
