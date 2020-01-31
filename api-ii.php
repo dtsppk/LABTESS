d2737951d4cc536b3a2e4e95afcb389cd41672ac
dtsppk <60294467+dtsppk@users.noreply.github.com>
2020-01-31 13:21:22 +0700

    Update api-ii.php

--- api-ii.php
+++ api-ii.php
@@ -33,7 +33,7 @@ if($arrJson['events'][0]['message']['text'] == "hi"){
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
  
- 
+}else{ 
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
